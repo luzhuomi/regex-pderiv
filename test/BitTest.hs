214a3d38b1c3038d6930fa5e529268bd34e36e7b
Kenny Zhuo Ming Lu <luzhuomi@gmail.com>
2023-03-07 13:18:41 +0800

    update

--- test/BitTest.hs
+++ test/BitTest.hs
@@ -1,14 +1,25 @@
 module Main where
 
 import System.Exit (exitFailure)
+import Test.Tasty (defaultMain, testGroup)
+import Test.Tasty.HUnit (assertEqual, testCase)
 
 import Text.Regex.PDeriv.BitCode.Transduce
 import Text.Regex.PDeriv.IntPattern
 import Text.Regex.PDeriv.Common
 
 
-main = do
+main = defaultMain unitTests
+
+
+unitTests =
+  testGroup
+    "Unit tests" [testBitCode]
+
+testBitCode = 
     let r4 = strip p4
         trans = buildTrans r4
         regex = buildRegex p4
-    putStrLn (show $ parseRegex regex "ABAAC" == parseBX trans r4 "ABAAC")
+    in testCase "parseRegex should be the same as parseBX" $ assertEqual [] (parseRegex regex "ABAAC") (parseBX trans r4 "ABAAC")
+
+
